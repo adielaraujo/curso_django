d50a0faa39184836ffc56c23c25fc31ff00a9c71
unknown <you@example.com>
2018-09-19 22:09:02 -0400

    ok

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'core',
 ]
 
@@ -131,4 +132,9 @@ TIME_INPUT_FORMATS = ('%H:%M:%S',)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
